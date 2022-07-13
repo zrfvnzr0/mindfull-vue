c3a6c2ba0ad9b868dbaa45eb048fcb6568a6292c
Paolo Licup <jpmlicup10@gmail.com>
2022-07-13 20:06:39 +0800

    v1

---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+// import views here
+
+const routes = [
+    // define paths here
+    {
+
+    },
+]
+
+const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes
+})
+
+export default router
